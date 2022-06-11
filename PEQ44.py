0951ac2a78e1ad1858ec1d1a855352f19133f710
winwin5037 <105581265+winwin5037@users.noreply.github.com>
2022-06-11 08:39:53 +0300

    Add files via upload

---
+++ PEQ44.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import time
+start_time = time.time()
+
+def pentagon(n): return n*((3*n)-1)/2
+i=6
+
+PentagonList = [1,5,12,22,35]
+while True:
+    newPentagon = pentagon(i)
+    PentagonList.append(newPentagon)
+    lastPentagon = PentagonList[-1]
+    for number in PentagonList:
+        if lastPentagon - number in PentagonList:
+            if abs(number-(lastPentagon-number)) in PentagonList:
+                D = abs(number-(lastPentagon-number))
+                print(D,number,lastPentagon-number)
+                print("--- %s seconds ---" % (time.time() - start_time))
+    i+=1
