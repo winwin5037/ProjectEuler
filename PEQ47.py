ee8dde7c92fc181a01837556db0cbc1b6dc7fa31
winwin5037 <105581265+winwin5037@users.noreply.github.com>
2022-06-18 09:37:19 +0300

    Add files via upload

---
+++ PEQ47.py
@@ -0,0 +1,28 @@
+import time
+start_time = time.time()
+
+def pf(n):
+    primeFactors = set()
+    while n % 2 == 0:
+        primeFactors.add(2)
+        n = n / 2
+    for i in range(3,int(n**(1/2))+1,2):
+        while (n % i == 0):
+            primeFactors.add(i)
+            n = n / i
+    if n > 2:
+        primeFactors.add(n)
+    return list(primeFactors)
+number = 10**5
+while True:
+    if len(pf(number))==4:
+        number+=1
+        if len(pf(number))==4:
+            number+=1
+            if len(pf(number))==4:
+                number +=1
+                if len(pf(number))==4:
+                    print(number-3)
+                    print("--- %s seconds ---" % (time.time() - start_time))
+                    break
+    number+=1
