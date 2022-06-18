ce3743beba54276bcea07e51abcded7878fd5e39
winwin5037 <105581265+winwin5037@users.noreply.github.com>
2022-06-18 10:46:53 +0300

    Add files via upload

---
+++ PEQ99/PEQ99.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[33]:
+
+
+import math
+
+def hmd(a,b):
+    return math.floor(b*math.log(a,10))+1
+
+finalList = list()
+maxValue = 0
+
+with open("p099_base_exp.txt","r") as numbers:
+    inputs = numbers.readlines()
+    for line in inputs:
+        line = line[:-1]
+        inputList = line.split(",")
+        a,b = int(inputList[0]),int(inputList[1])
+        temp = hmd(a,b)
+        if temp == 3005316:
+            print([a,b,temp])
+
+
+# In[ ]:
+
+
+
+
