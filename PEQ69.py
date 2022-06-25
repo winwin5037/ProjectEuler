e3b4ee15af716d3ac34fed350d94dbe3d1b05ff6
winwin5037 <105581265+winwin5037@users.noreply.github.com>
2022-06-25 14:19:17 +0300

    Update PEQ69.py

--- PEQ69.py
+++ PEQ69.py
@@ -1,12 +1,14 @@
 import time
 start_time = time.time()
 
-def isprime(n):
-    if n<2:
+def isprime(num):
+    if num==2 or num==3:
+        return True
+    if num%2==0 or num<2:
         return False
-    for i in range(2,int(n**(1/2))+1):
-        if n%i==0:
-            return False
+    for n in range(3,int(num**0.5)+1,2):   
+        if num%n==0:
+            return False   
     return True
         
 
