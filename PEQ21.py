0f77dd1211e8398175e7410492b5ce1f6ecb1d44
winwin5037 <105581265+winwin5037@users.noreply.github.com>
2022-06-27 16:12:49 +0300

    Add files via upload

---
+++ PEQ21.py
@@ -0,0 +1,17 @@
+def divisors(n):
+    for i in range(1, int(n/2) + 1):
+        if n % i == 0:
+            yield i
+    yield n
+
+def d(n):
+    return sum(divisors(n))-n
+i=2
+total = 0
+while True:
+    if d(d(i))==i and i!=d(i):
+        total +=i
+    i+=1
+    if i>10**4:
+        break
+print(total)
