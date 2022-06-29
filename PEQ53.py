3e09ab3047f6d543b34717fd154ba14f3f6b8bcc
winwin5037 <105581265+winwin5037@users.noreply.github.com>
2022-06-29 16:23:46 +0300

    Update PEQ53.py

--- PEQ53.py
+++ PEQ53.py
@@ -1,9 +1,9 @@
-import math
+from math import comb
 
 count = 0
 
 for i in range(0,101):
     for j in range(0,i+1):
-        if math.comb(i,j)>10**6:
+        if comb(i,j)>10**6:
             count+=1
 print(count)
