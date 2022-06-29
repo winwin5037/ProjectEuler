8a66b87bffa70cf211ed376cbcb02894bc3bd3d6
winwin5037 <105581265+winwin5037@users.noreply.github.com>
2022-06-29 16:19:28 +0300

    Update PEQ63.py

--- PEQ63.py
+++ PEQ63.py
@@ -4,12 +4,14 @@ finalList = list(range(1,10))
 while True:
     if len(str(pow(i,j)))==j:
         finalList.append(i**j)
-        print(i,j,i**j,f"the answer is {len(finalList)}")
     i+=1
     if len(str(pow(i,j)))>j or i==10:
         i=0
         j+=1
         continue
+    if j>30:
+        break
+print(len(finalList))
 
 # After the printed line 9 21 109418989131512359209, 
 # you will see that there is no more output here
