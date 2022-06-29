94514d0c3bb91052a16bae910a57bda80a60d66d
winwin5037 <105581265+winwin5037@users.noreply.github.com>
2022-06-29 16:23:04 +0300

    Update PEQ53.py

--- PEQ53.py
+++ PEQ53.py
@@ -1,10 +1,9 @@
 import math
-import numpy as np
 
-finalList = np.array([])
+count = 0
 
 for i in range(0,101):
     for j in range(0,i+1):
-        finalList = np.append(finalList,math.comb(i,j))
-
-print(len(finalList[finalList > 10**6]))
+        if math.comb(i,j)>10**6:
+            count+=1
+print(count)
